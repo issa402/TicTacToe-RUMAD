710cebc4eb21a4223721d279f229cb7f502c7b5f
issa402 <65190332+issa402@users.noreply.github.com>
2024-04-02 21:49:06 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.yourcompany.tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        //Instance for gamee
+        TicTacToe game = new TicTacToe();
+        
+        // Start
+        game.play();
+    }
+}
